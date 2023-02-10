8d5e06709a0c25783af3e75466f381172baa04d1
Serge Shliayonkin <sshliayonkin@scnsoft.com@SSHLIAYONKIN-MB.SCNSOFT.COM>
2023-02-10 11:45:08 +0300

    test submodule

--- cmd/k8s-integrity-sum/main.go
+++ cmd/k8s-integrity-sum/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
-	"github.com/integrity-sum/internal/configs"
-	logConfig "github.com/integrity-sum/pkg/logger"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/integrity-sum/internal/configs"
+	logConfig "github.com/integrity-sum/pkg/logger"
 
 	"github.com/integrity-sum/internal/initialize"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	return
+
 	// Load values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("No .env file found")
